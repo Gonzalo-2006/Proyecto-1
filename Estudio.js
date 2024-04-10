1eb2aa41c2ab662c83a9c860e6a17048745f2a3d
Andrey <andreyportela187@gmail.com>
2024-04-09 20:48:10 -0500

    Inicio

---
+++ Estudio.js
@@ -0,0 +1,178 @@
+
+// Interaccion 
+//Preguntamos el nombre y lo guardamos en una variable
+  /*let mensaje = prompt("Cual es tu nombre", "")
+  console.log(mensaje)
+  */
+
+//Devolvemos ese valor
+  //alert(mensaje)
+
+//Preguntamos si el valor es correcto
+  //var mensaje2 = confirm(`¿Tu eres ${mensaje}?`)
+
+// Conversor de tipo
+ //Tipo string
+  /*
+   let valor = true;
+   alert(typeof valor)
+
+   valor = String(valor)
+   alert(typeof valor)
+  */
+ //Tipo numbres 
+  /*
+   let valor = "123"
+   console.log(typeof valor)
+   console.log(valor)
+
+   valor = Number(valor)
+   console.log(typeof valor)
+   console.log(valor)
+ */
+ // Tipo boolean 
+  /*
+   let dato = ""
+   console.log(typeof dato)
+   dato = Boolean(dato)
+   alert(dato)
+   console.log(typeof dato)
+   1 = true 
+   0 = false
+   "Hola" = true 
+   "" = false 
+  */
+ // Condicionales
+   // Condicional ternario: ? 
+  /*
+   let edad = parseInt(prompt("Edad?"))
+   console.log(`Tu edad es ${edad}`)
+   
+   let paso = (edad < 5)? console.log("No pasa"): 
+   (edad >= 5 && edad < 12)? console.log("Posible"): 
+   (edad >= 13 && edad < 18)? console.log("Estas cerca"):
+   (edad >= 18)? console.log("Pasa"): alert("No hay entradas")
+  */
+
+   //Un ejemplo de algoritmo 
+   /*
+   function decoded(encodedTxt) {
+    let alphabet = "abcdefghijklmnñopqrstuvwxyz".split("");
+    let decodedTxt = "";
+    encodedTxt.toLowerCase().split("").forEach(item => {
+        if (item === " ") {
+            decodedTxt += item;
+         }
+        else if (alphabet.indexOf(item) != -1) {
+            if (alphabet.indexOf(item) >= 5) {
+                decodedTxt += alphabet[(alphabet.indexOf(item) - 5)];
+            }
+            else if (alphabet.indexOf(item) <= 4) {
+                decodedTxt += alphabet[alphabet.length + alphabet.indexOf(item) - 5];
+            }
+         }
+     });
+     console.log(decodedTxt.toUpperCase());
+   }
+   const txtEncrypted = 'KJPN INF IJ PTX UWTLWFQFITWJX';
+   decoded(txtEncrypted);
+   
+  // Pedir el nombre del usuario
+var nombreUsuario = prompt("Por favor, introduce tu nombre:");
+
+// Obtener el elemento h1
+var h1Elemento = document.getElementById("nombreUsuario");
+
+// Actualizar el contenido del h1 con el nombre del usuario
+h1Elemento.textContent = "¡Hola, " + nombreUsuario + "!";
+
+// Orientada a objetos POO
+
+var lista = {
+  nombre: "andrey",
+  apellido: "portela",
+  deportes: ["correr","futbol","tenis"]
+}
+
+var lista2 = ["Gonzalo","Ortiz",{deporte: "caminar",peliculas: "acción"}]
+
+function fulNAme(){
+  var dato = this.lista.nombre+" "+this.lista.apellido
+  return dato
+}
+console.log(fulNAme())
+
+
+var tarjeta = {
+  //Properties o propiedades
+  proietario: "Andrey",
+  edadDePropietario: 18,
+  montoTotal: 100+" col", 
+
+  //Method o Aciones/Funciones
+  deposito(depos){
+    return (this.montoTotal = parseInt(this.montoTotal) + depos +" col")
+
+  },
+  retiro(ret){
+    return this.montoTotal = parseInt(this.montoTotal) - ret +" col"
+  }
+}*/
+
+class Tarjeta {
+  constructor() {
+    this.proietario ="",
+    this.edadDePropietario =0,
+    this.montoTotal = 0 +" col",
+
+    this.depositos = function(depos){
+      return (this.montoTotal = parseInt(this.montoTotal) + depos +" col")}
+
+    this.retiro = function(ret){
+      return this.montoTotal = parseInt(this.montoTotal) - ret +" col"}
+    }
+}
+
+var user1 = new Tarjeta()
+  user1.proietario = "Andrey"
+  user1.edadDePropietario = 30
+  user1.montoTotal = 600
+
+var user2 = new Tarjeta()
+  user2.proietario = "Andy"
+  user2.edadDePropietario = 18
+  user2.montoTotal = 600;
+
+var user3 = new Tarjeta()
+  user3.proietario = "Chalo"
+  user3.edadDePropietario = 30 
+  user3.montoTotal = 900;
+
+console.log("Usuario 1: ",user1)
+console.log("Usuario 2: ",user2)
+console.log("Usuario 3: ",user3)
+
+class Bosillo extends Tarjeta{
+  constructor(){
+    super(Tarjeta)
+    this.nombreDelBolsillo = ""
+    this.montoDeBolsillo = 0
+    this.ladoDelBolsillo = ""
+  }
+}
+
+var user4 = new Bosillo()
+//Datos del Usuario
+  user4.proietario = user1.proietario 
+  user4.edadDePropietario = user1.edadDePropietario
+  user4.montoTotal = user1.montoTotal
+  
+
+//Datos del bolsillo 
+  user4.tamañoDeBolsillo = 40
+  user4.nombreDelBolsillo = "Pedrito"
+  user4.ladoDelBolsillo = "Derecho"
+
+  user4.montoDeBolsillo  = parseInt(user1.montoTotal += user4.tamañoDeBolsillo)
+ 
+console.log("Usuario 4: ",user4)
---
+++ Practica.js
@@ -0,0 +1,184 @@
+//Inicio de js
+
+//Funciones: 
+
+function color (){
+    var texto = document.getElementById('texto')
+    texto.style.color = '#' + Math.floor(Math.random()*16777215).toString(16);
+}
+/*
+
+print = descarga de document. 
+ let nombre = "Andrey"
+ nombre = "Gonzalo"
+ alert(nombre)
+
+
+function comparar(a,b){
+   if(a>b){
+    return ("a es mayor que b")
+   }else if(a===b){
+    return ("a es igual que b")
+   }else if(a<b){
+    return (" a es menor que b")
+   }
+}
+console.log(comparar(3,6))
+
+
+function comparar2(a, b) {
+    let compara = a === b ? 'iguales' : a > b ? 'mayor' : 'menor';
+    switch (compara) {
+      case 'iguales':
+        return("a es igual que b");
+        break;
+      case 'mayor':
+        return("a es mayor que b");
+        break;
+      case 'menor':
+        return("a es menor que b");
+        break;
+      default:
+        return("No es posible comparar");
+        break;
+    }
+  }
+
+  console.log(comparar2(5, 10))
+  console.log(comparar2(10, 5))
+  console.log(comparar2(5, 5))
+*/
+
+// Interaccion
+/*
+var alerta = alert("Hola")
+var promPt = prompt("Nombre")
+var confirmar = confirm("Estas vivo?")
+
+let cantidadDeDatos = prompt("Ingrese la cantidad de datos a registrar")
+let lisataDeDatos  = []
+for(a=0; a < cantidadDeDatos; a++){
+  lisataDeDatos[a] = [prompt(`Ingrese el dato N°${a+1}:`)]
+}
+console.log(lisataDeDatos)
+
+if(a == 2){
+  return(mensaje(a))
+}else{
+  alert("Funcion no activa")
+}
+
+
+function mensaje(dato1){
+  if( dato1== 2){
+    return alert("Hola")
+  }else{
+    return ("sapa")
+  }
+}
+
+let count = 0;
+var holdbet = "Hold";
+function cc(card) {
+  switch(card){
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+    count ++;
+    break;
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+    count --;
+    break;
+  }
+
+  if (count > 0) {
+    holdbet = 'Bet'
+  }
+  return (count +""+holdbet)
+}
+console.log(cc(2)) 
+console.log(cc(3))
+console.log(cc(7))
+console.log(cc('K')) 
+console.log(cc('A'))
+
+function almacen(val){
+  const dato = {
+    "ciudad": "cali",
+    "comida": "pollo",
+    "edad": 13
+  }
+  resultado = dato[val]
+  return resultado
+}
+document.write(almacen("comida"))
+
+
+*/  
+
+function verDato(){
+  var dato = document.getElementById("nombre").value; 
+  var dato2 = document.getElementById("apellido").value; 
+  alert(dato +" "+dato2)
+}
+/*
+let listComi = {
+  pera: "pera",
+  sopa: "sopa",
+  jugo: "jugo"
+} 
+
+let comida = [];
+comida.push("platano","banano","pera")
+console.log(comida.length)
+
+
+function ver(){
+  alert("hola")
+}
+
+console.log(listComi.pera)
+*/
+/*
+function compara(){
+  let dato = parseInt(prompt("ingrese un numero entre 1 y 5"))
+  const matriz = [1,2,3,4,5]
+  const compaRa = matriz.includes(dato)
+  if(compaRa){
+    return true
+  }else{
+    return false
+  }
+}
+console.log(compara())
+
+
+const a = [1,2,3,4,5]
+const b = [1,2,3,4,5]
+const c = "1,2,3,4,5"
+console.log(a)
+a.push(6)
+console.log(a.length)
+console.log(a)
+console.log(a.length)
+
+let dato = true
+console.log(dato)
+
+dato = String(dato)
+console.log(dato)
+
+dato = true
+dato = Number(dato)
+console.log(dato)
+*/
+  
+
+
+ 
